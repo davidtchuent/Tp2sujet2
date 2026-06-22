27f92e22a2af61f687ddddd1b3bcc4f9e9c0c629
kydlaugan <kaptchouangyvand@gmail.com>
2026-06-22 22:09:39 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+
+"""
+TP Deep Learning – Application Streamlit complète
+====================================================
+
+
+Par: Tchuenteu Guetchueng David – 20U2891 – Info4
+Et: Nafiesatou Hamadicko 20U2891
+Sous la supervision de Stéphane C. K. TÉKOUABOU (PhD & Ing.) - UY1 2025-2026
+"""
